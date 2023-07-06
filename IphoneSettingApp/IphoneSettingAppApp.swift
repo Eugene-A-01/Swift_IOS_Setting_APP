f235d9114fe5566d379f37ea20fdbc3e47a8131e
Eujin-A-01 <eugene01@yonsei.ac.kr>
2023-07-06 15:52:09 +0900

    Initial Commit

---
+++ IphoneSettingApp/IphoneSettingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneSettingAppApp.swift
+//  IphoneSettingApp
+//
+//  Created by Eugene on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct IphoneSettingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
